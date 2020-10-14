78490174abd11270c5dbb9e215f928c77adb72f3
Agung Cahya Gumilar <gumilar.agungcahya>
2020-10-14 17:56:23 +0700

    forOption in fuction service

--- services/serviceGlobal.js
+++ services/serviceGlobal.js
@@ -75,21 +75,26 @@ const serviceGloba = {
         await axios.get(
             consConfig.urlService + 'fixture-type',
             {   
+                params: {
+                    forOption: true,
+                },
                 headers: Token 
             })
             .then(function(response) {
-                temp  = response.data.data.map(obj => {
-                    var tempData = {
-                        id : obj.id,
-                        text : obj.fixture_type
-                    }
-                    return tempData;
-                });
+                temp = response.data.data
+                // temp  = response.data.data.map(obj => {
+                //     var tempData = {
+                //         id : obj.id,
+                //         text : obj.fixture_type
+                //     }
+                //     return tempData;
+                // });
                 console.log("success service getAllSelectFixtureType")
             })
             .catch(function (error) {
                 console.log("error service getAllSelectFixtureType")
             });
+        console.log(temp)
         return temp;
     },
     getAllSelectStore: async (Token) => {
@@ -98,16 +103,20 @@ const serviceGloba = {
         await axios.get(
             consConfig.urlService + 'store',
             {   
+                params: {
+                    forOption: true,
+                },
                 headers: Token 
             })
             .then(function(response) {
-                temp  = response.data.data.map(obj => {
-                    var tempData = {
-                        id : obj.store_code,
-                        text : obj.store_code
-                    }
-                    return tempData;
-                });
+                temp = response.data.data
+                // temp  = response.data.data.map(obj => {
+                //     var tempData = {
+                //         id : obj.store_code,
+                //         text : obj.store_code
+                //     }
+                //     return tempData;
+                // });
                 console.log("success service getAllSelectStore")
             })
             .catch(function (error) {
@@ -121,16 +130,20 @@ const serviceGloba = {
         await axios.get(
             consConfig.urlService + 'dc',
             {   
+                params: {
+                    forOption: true,
+                },
                 headers: Token 
             })
             .then(function(response) {
-                temp  = response.data.data.map(obj => {
-                    var tempData = {
-                        id : obj.id,
-                        text : obj.DC_name
-                    }
-                    return tempData;
-                });
+                temp = response.data.data
+                // temp  = response.data.data.map(obj => {
+                //     var tempData = {
+                //         id : obj.id,
+                //         text : obj.DC_name
+                //     }
+                //     return tempData;
+                // });
                 console.log("success service getAllSelectDC")
             })
             .catch(function (error) {
@@ -144,16 +157,20 @@ const serviceGloba = {
         await axios.get(
             consConfig.urlService + 'user',
             {   
+                params: {
+                    forOption: true,
+                },
                 headers: Token 
             })
             .then(function(response) {
-                temp  = response.data.data.map(obj => {
-                    var tempData = {
-                        id : obj.id,
-                        text : obj.nik
-                    }
-                    return tempData;
-                });
+                temp = response.data.data
+                // temp  = response.data.data.map(obj => {
+                //     var tempData = {
+                //         id : obj.id,
+                //         text : obj.nik
+                //     }
+                //     return tempData;
+                // });
                 console.log("success service getAllSelectMD")
             })
             .catch(function (error) {
@@ -167,16 +184,20 @@ const serviceGloba = {
         await axios.get(
             consConfig.urlService + 'retailer',
             {   
+                params: {
+                    forOption: true,
+                },
                 headers: Token 
             })
             .then(function(response) {
-                temp  = response.data.data.map(obj => {
-                    var tempData = {
-                        id : obj.id,
-                        text : obj.retailer_name
-                    }
-                    return tempData;
-                });
+                temp = response.data.data
+                // temp  = response.data.data.map(obj => {
+                //     var tempData = {
+                //         id : obj.id,
+                //         text : obj.retailer_name
+                //     }
+                //     return tempData;
+                // });
                 console.log("success service getAllSelectRetailer")
             })
             .catch(function (error) {
